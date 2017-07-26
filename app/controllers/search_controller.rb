d563842f5f074e3acc30811407295481f85b2d21
NataliaColome <nmcolome@gmail.com>
2017-07-26 10:55:23 -0600

    change test to match 10 stores, create Faraday request in SearchController

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
   def index
-    
+    binding.pry
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{params[:search]},25))?format=json&show=longName,city,distance,phone,storeType&apiKey=#{ENV['best_buy_api_key']}")
+    response_json = JSON.parse(response.body, symbolize_names: true)
+    total_stores = response_json[:total]
+    @stores = response_json[:stores]
+    [:longName]
+    binding.pry
   end
 end
