667a288c264101ffa3d8b4bb8f78f054797ed560
NataliaColome <nmcolome@gmail.com>
2017-07-26 12:08:27 -0600

    change test to reflect pagination functionality, change method in  header in view to use 'total_count'

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,5 @@
 class SearchController < ApplicationController
   def index
-    # @stores = Store.get_stores_near_me(params[:search])
     @stores = Kaminari.paginate_array(Store.get_stores_near_me(params[:search])).page(params[:page]).per(10)
   end
 end
